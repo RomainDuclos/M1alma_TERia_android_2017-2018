458934c819ecf2b6f1102245cadb33f072ed65f3
RomainDuclos <33464490+RomainDuclos@users.noreply.github.com>
2018-04-20 20:54:13 +0200

    Update RoundingJNI.java

--- JNIRounding/RoundingJNI.java
+++ JNIRounding/RoundingJNI.java
@@ -1,9 +1,7 @@
 public class RoundingJNI
 {
 
-	static {
-		System.loadLibrary("RoundingLib");
-	}
+	
 
 	public native void round_down(); //lance le code C/C++ de fesetround(down)
 	public native void round_up(); //lance le code C/C++ de fesetround(up)
