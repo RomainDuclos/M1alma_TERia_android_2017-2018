62f73ec77b7deb327aa66394b47895848f2f43b5
RomainDuclos <33464490+RomainDuclos@users.noreply.github.com>
2018-04-20 20:51:09 +0200

    Update RoundingJNI.java

--- JNIRounding/RoundingJNI.java
+++ JNIRounding/RoundingJNI.java
@@ -8,19 +8,6 @@ public class RoundingJNI
 	public native void round_down(); //lance le code C/C++ de fesetround(down)
 	public native void round_up(); //lance le code C/C++ de fesetround(up)
 
-	public static double f()
-	{
-		return 10.0;
-	}
-
-	public static void main(String[] args)
-	{
-		new RoundingJNI().round_down();
-		double x = 1.0 / f();
-		new RoundingJNI().round_up();
-		double y = 1.0 / f();
-		boolean t = x==y;
-		System.out.println(" x = y ? : " + t);
-	}
+	
 	
 }
